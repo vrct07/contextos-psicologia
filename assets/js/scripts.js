e77a7c10f045a1c304684fb49425b1b4ee720d00
vrct07 <vrct07@gmail.com>
2021-06-20 21:08:01 -0300

    finish

---
+++ assets/js/scripts.js
@@ -0,0 +1,148 @@
+$(document).ready(function(){
+    
+    // Progress bar
+    let containerA = document.getElementById("circleA");
+
+    let circleA = new ProgressBar.Circle(containerA, {
+
+        color: '#FFFFFF',
+        strokeWidth: 8,
+        duration: 1400,
+        from: { color: '#AAA' },
+        to: { color: '#4e8174'},
+
+        step: function(state, circle) {
+
+            circle.path.setAttribute('stroke', state.color);
+
+            let value = Math.round(circle.value() * 60);
+
+            circle.setText(value);
+        }
+
+    });
+
+    let containerB = document.getElementById("circleB");
+
+    let circleB = new ProgressBar.Circle(containerB, {
+
+        color: '#FFFFFF',
+        strokeWidth: 8,
+        duration: 1600,
+        from: { color: '#AAA' },
+        to: { color: '#4e8174'},
+
+        step: function(state, circle) {
+
+            circle.path.setAttribute('stroke', state.color);
+
+            let value = Math.round(circle.value() * 254);
+
+            circle.setText(value);
+        }
+
+    });
+
+    let containerC = document.getElementById("circleC");
+
+    let circleC = new ProgressBar.Circle(containerC, {
+
+        color: '#FFFFFF',
+        strokeWidth: 8,
+        duration: 2000,
+        from: { color: '#AAA' },
+        to: { color: '#4e8174'},
+
+        step: function(state, circle) {
+
+            circle.path.setAttribute('stroke', state.color);
+
+            let value = Math.round(circle.value() * 32);
+
+            circle.setText(value);
+        }
+
+    });
+
+    let containerD = document.getElementById("circleD");
+
+    let circleD = new ProgressBar.Circle(containerD, {
+
+        color: '#FFFFFF',
+        strokeWidth: 8,
+        duration: 2200,
+        from: { color: '#AAA' },
+        to: { color: '#4e8174'},
+
+        step: function(state, circle) {
+
+            circle.path.setAttribute('stroke', state.color);
+
+            let value = Math.round(circle.value() * 15);
+
+            circle.setText(value);
+        }
+
+    });
+
+    // iniciando o loader quando o usuário chega no elemento.
+    let dataAreaOffset = $('#data-area').offset();
+    let stop = 0;
+
+    $(window).scroll(function(e) {
+
+        let scroll = $(window).scrollTop();
+
+        if(scroll > (dataAreaOffset.top - 500) && stop == 0) {
+            circleA.animate(1.0);
+            circleB.animate(1.0);
+            circleC.animate(1.0);
+            circleD.animate(1.0);
+
+            stop = 1;
+
+        }
+
+    });
+
+    // Parallax
+    setTimeout(function() {
+        
+        $('#data-area').parallax({imageSrc: 'img/parallax-1.png'});
+
+    }, 250);
+
+
+    // scroll para seções
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#services-area');
+    let consultSection = $('#consult-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function() {
+
+        let btnId = $(this).attr('id');
+
+        if(btnId == 'about-menu') {
+            scrollTo = aboutSection;
+        } else if(btnId == 'services-menu') {
+            scrollTo = servicesSection;
+        } else if(btnId == 'consult-menu') {
+            scrollTo = consultSection;
+        } else if(btnId == 'contact-menu') {
+            scrollTo = contactSection;
+        } else {
+            scrollTo = bannerSection;
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70
+        }, 1500);
+    });
+
+});
